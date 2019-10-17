6f521ee69af703b12db12d7dd6f0445c52ffa400
Rachael Homann <rachaelhomann@gmail.com>
2019-10-17 14:36:18 -0500

    added table, jumbotron, and styling to both

---
+++ app.js
@@ -0,0 +1,10 @@
+var saveBtn = document.getElementById="button-addon2";//today's date
+
+//helper functions
+
+//events
+
+//init
+ /*check local storage object for matching id*/
+    /*set today's date in header*/
+    /*color code past/current/future bars*/
